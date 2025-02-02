0ee3f9030d30065b608bb8fff287fa8aba1aec42
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2025-02-02 23:04:59 +0300

    Delete frontend/starter.ps1

--- frontend/starter.ps1
+++
@@ -1,2 +0,0 @@
-Start-Process -NoNewWindow -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File backend.ps1"
-Start-Process -NoNewWindow -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File frontend.ps1"
